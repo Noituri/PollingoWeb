e5042de7fcbb161758868ba7686ab664c49c0abd
yknomeh <yknomeh@protonmail.com>
2018-09-29 15:22:33 +0200

    Happy B-Day

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,35 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div> -->
     <router-view/>
   </div>
 </template>
 
 <style>
+@font-face {
+  font-family: ProductSans;
+  src: url('./assets/fonts/Product\ Sans\ Regular.ttf');
+}
+
+html {
+  background: linear-gradient(45deg,#DA1FF2,#4C15D0) no-repeat center center fixed;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'ProductSans', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #fff;
 }
+
+.title {
+  font-size: 62px;
+} 
+
 #nav {
   padding: 30px;
 }
---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <h1 class="title">Pollingo</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HomeComponent extends Vue {
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
