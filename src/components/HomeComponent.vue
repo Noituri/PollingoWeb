fa56202d321efa3952b457f7d01ca940ccce6691
yknomeh <yknomeh@protonmail.com>
2018-09-29 18:39:13 +0200

    bulma

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="home">
-    <h1 class="title">Pollingo</h1>
+    <h1 class="title">{{ appName }}</h1>
+
+    <transition name="slide-fade">
+      <div v-if="showLoginDialog" class="loginDialog">
+        <h3>Sign In</h3>
+        <input class="input is-rounded loginDialog-Content" type="text" placeholder="Rounded input">
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -9,11 +16,58 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component
 export default class HomeComponent extends Vue {
+  @Prop() private appName!: string;
+  private data() {
+    return {
+      showLoginDialog: false,
+    };
+  }
 
+  private mounted() {
+    this.$nextTick(async () => {
+      await new Promise( (resolve) => setTimeout(resolve, 300) );
+      this.$data.showLoginDialog = true;
+    });
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
+.loginDialog {
+    /* display: none; */
+    position: fixed;
+    z-index: 1;
+    padding-top: 10px;
+    left: 25%;
+    top: 25%;
+    width: 50%;
+    height: 50%;
+    overflow: hidden;
+    border-radius: 25px;
+    box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.1);
+    background-color: rgb(255, 255, 255);
+    color: #000;
+    font-size: 20px;
+ }
+
+.loginDialog-Content {
+  margin-left: 2px;
+  margin-right: 2px;
+  box-shadow: none;
+}
+
+.slide-fade-enter-active {
+  transition: all .5s ease;
+}
+
+.slide-fade-leave-active {
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+
+.slide-fade-enter, .slide-fade-leave-to {
+  transform: translateY(10px);
+  opacity: 0;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <HomeComponent/>
+    <HomeComponent v-bind:appName="title"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HomeComponent from '@/components/HomeComponent.vue'; // @ is an alias to /src
+import Config from '@/config/config';
 
 @Component({
   components: {
     HomeComponent,
+  },
+  data() {
+    return {
+      title: Config.AppName,
+    };
   },
 })
 export default class Home extends Vue {}
